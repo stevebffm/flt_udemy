4ed2933cf4e30327ad2d130c323725f9e105c729
Steve <sylvius4@gmail.com>
2020-12-28 15:29:28 +0900

    process..

---
+++ shop_app/lib/screens/products_overview_screen.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+import '../widgets/products_grid.dart';
+
+class ProductsOverviewScreen extends StatelessWidget {
+  // final List<Product> loadedProducts = ;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('MyShop'),
+      ),
+      body: ProductsGrid(),
+    );
+  }
+}
---
+++ shop_app/lib/widgets/product_item.dart
@@ -0,0 +1,46 @@
+import 'package:flutter/material.dart';
+import '../screens/product_detail_screen.dart';
+import '../providers/product.dart';
+import 'package:provider/provider.dart';
+
+class ProductItem extends StatelessWidget {
+  // final String id;
+  // final String title;
+  // final String imageUrl;
+
+  // ProductItem(this.id, this.title, this.imageUrl);
+
+  @override
+  Widget build(BuildContext context) {
+    final product = Provider.of<Product>(context);
+    return ClipRRect(
+      borderRadius: BorderRadius.circular(10),
+      child: GridTile(
+        child: GestureDetector(
+          onTap: () {
+            Navigator.of(context)
+                .pushNamed(ProductDetailScreen.routeName, arguments: id);
+          },
+          child: Image.network(
+            product.imageUrl,
+            fit: BoxFit.cover,
+          ),
+        ),
+        footer: GridTileBar(
+          backgroundColor: Colors.black54,
+          leading: IconButton(
+            icon: Icon(Icons.favorite),
+            color: Theme.of(context).accentColor,
+            onPressed: () {},
+          ),
+          trailing: IconButton(
+            icon: Icon(Icons.shopping_cart),
+            color: Theme.of(context).accentColor,
+            onPressed: () {},
+          ),
+          title: Text(product.title, textAlign: TextAlign.center),
+        ),
+      ),
+    );
+  }
+}
